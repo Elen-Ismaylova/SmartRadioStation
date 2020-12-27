90b9c3d900a502c0f270709c1a55568676eed9a8
Elen Ismaylova <wonderelen@gmail.com>
2020-12-27 17:37:05 +0300

    new

---
+++ src/test/java/StationTest.java
@@ -0,0 +1,38 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StationTest {
+
+    @Test
+    void getCurrentStation() {
+    }
+
+    @Test
+    void setCurrentStation() {
+    }
+
+    @Test
+    void nextStation() {
+    }
+
+    @Test
+    void previousStation() {
+    }
+
+    @Test
+    void getCurrentVolume() {
+    }
+
+    @Test
+    void setCurrentVolume() {
+    }
+
+    @Test
+    void increaseVolume() {
+    }
+
+    @Test
+    void decreaseVolume() {
+    }
+}
